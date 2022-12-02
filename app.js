81f190c893159ee134e3b2cb6dacd4094d4d5153
Ayush Ak <khadgiayush136@gmail.com>
2022-12-03 01:21:22 +0530

    add

--- app.js
+++ app.js
@@ -31,146 +31,146 @@ app.get('/location',(req,res) => {
         if(err) throw err;
         res.send(result)
     })
-})
+ })
 
-// list of restaurant
-app.get('/restaurant',(req,res) => {
-    let query = {};
-    let stateId = Number(req.query.stateId);
-    let mealId = Number(req.query.mealId);
-    if(stateId){
-        query={state_id:stateId}
-    }else if(mealId){
-        query={"mealTypes.mealtype_id":mealId}
-    }
-    db.collection('Restaurantdata').find(query).toArray((err,result) =>{
-        if(err) throw err;
-        res.send(result)
-    })
-})
+// // list of restaurant
+// app.get('/restaurant',(req,res) => {
+//     let query = {};
+//     let stateId = Number(req.query.stateId);
+//     let mealId = Number(req.query.mealId);
+//     if(stateId){
+//         query={state_id:stateId}
+//     }else if(mealId){
+//         query={"mealTypes.mealtype_id":mealId}
+//     }
+//     db.collection('Restaurantdata').find(query).toArray((err,result) =>{
+//         if(err) throw err;
+//         res.send(result)
+//     })
+// })
 
-app.get('/filter/:mealId',(req,res) => {
-    let query = {};
-    let sort = {cost:1}
-    let mealId = Number(req.params.mealId);
-    let cuisineId = Number(req.query.cuisineId);
-    let lcost = Number(req.query.lcost);
-    let hcost = Number(req.query.hcost);
-    if(req.query.sort){
-        sort={cost:req.query.sort}
-    }
-    if(cuisineId && lcost && hcost){
-        query={
-            "mealTypes.mealtype_id":mealId,
-            "cuisines.cuisine_id":cuisineId,
-            $and:[{cost:{$gt:lcost,$lt:hcost}}]
-        }
-    }
-    else if(cuisineId){
-        query={
-            "mealTypes.mealtype_id":mealId,
-            "cuisines.cuisine_id":cuisineId
-        }
-    }else if(lcost && hcost){
-        query={
-            "mealTypes.mealtype_id":mealId,
-            $and:[{cost:{$gt:lcost,$lt:hcost}}]
-        } 
-    }
-    db.collection('Restaurantdata').find(query).sort(sort).toArray((err,result) =>{
-        if(err) throw err;
-        res.send(result)
-    })
-})
+// app.get('/filter/:mealId',(req,res) => {
+//     let query = {};
+//     let sort = {cost:1}
+//     let mealId = Number(req.params.mealId);
+//     let cuisineId = Number(req.query.cuisineId);
+//     let lcost = Number(req.query.lcost);
+//     let hcost = Number(req.query.hcost);
+//     if(req.query.sort){
+//         sort={cost:req.query.sort}
+//     }
+//     if(cuisineId && lcost && hcost){
+//         query={
+//             "mealTypes.mealtype_id":mealId,
+//             "cuisines.cuisine_id":cuisineId,
+//             $and:[{cost:{$gt:lcost,$lt:hcost}}]
+//         }
+//     }
+//     else if(cuisineId){
+//         query={
+//             "mealTypes.mealtype_id":mealId,
+//             "cuisines.cuisine_id":cuisineId
+//         }
+//     }else if(lcost && hcost){
+//         query={
+//             "mealTypes.mealtype_id":mealId,
+//             $and:[{cost:{$gt:lcost,$lt:hcost}}]
+//         } 
+//     }
+//     db.collection('Restaurantdata').find(query).sort(sort).toArray((err,result) =>{
+//         if(err) throw err;
+//         res.send(result)
+//     })
+// })
 
-// list of meals
-app.get('/meals',(req,res) => {
-    db.collection('mealtypes').find().toArray((err,result) =>{
-        if(err) throw err;
-        res.send(result)
-    })
-})
+// // list of meals
+// app.get('/meals',(req,res) => {
+//     db.collection('mealtypes').find().toArray((err,result) =>{
+//         if(err) throw err;
+//         res.send(result)
+//     })
+// })
 
-//restaurant details
-app.get('/details/:id',(req,res) => {
-    //let id = mongo.ObjectId(req.params.id)
-    let id = Number(req.params.id)
-    db.collection('Restaurantdata').find({restaurant_id:id}).toArray((err,result) =>{
-        if(err) throw err;
-        res.send(result)
-    })
-})
+// //restaurant details
+// app.get('/details/:id',(req,res) => {
+//     //let id = mongo.ObjectId(req.params.id)
+//     let id = Number(req.params.id)
+//     db.collection('Restaurantdata').find({restaurant_id:id}).toArray((err,result) =>{
+//         if(err) throw err;
+//         res.send(result)
+//     })
+// })
 
-//restaurant menu details
-app.get('/menu/:id',(req,res) => {
-    let id = Number(req.params.id)
-    db.collection('RestaurantMenu').find({restaurant_id:id}).toArray((err,result) =>{
-        if(err) throw err;
-        res.send(result)
-    })
-})
+// //restaurant menu details
+// app.get('/menu/:id',(req,res) => {
+//     let id = Number(req.params.id)
+//     db.collection('RestaurantMenu').find({restaurant_id:id}).toArray((err,result) =>{
+//         if(err) throw err;
+//         res.send(result)
+//     })
+// })
 
-app.post('/placeOrder',(req,res) => {
-    console.log(req.body);
-    db.collection('orders').insert(req.body,(err,result) => {
-        if(err) throw err;
-        res.send('Order Placed')
-    })
-})
+// app.post('/placeOrder',(req,res) => {
+//     console.log(req.body);
+//     db.collection('orders').insert(req.body,(err,result) => {
+//         if(err) throw err;
+//         res.send('Order Placed')
+//     })
+// })
 
-// //menu details
-app.post('/menuItem',(req,res) => {
-    if(Array.isArray(req.body.id)){
-        db.collection('RestaurantMenu').find({menu_id:{$in:req.body.id}}).toArray((err,result) =>{
-            if(err) throw err;
-            res.send(result)
-        })
-    }else{
-        res.send('Inavlid Input')
-    }
-})
+// // //menu details
+// app.post('/menuItem',(req,res) => {
+//     if(Array.isArray(req.body.id)){
+//         db.collection('RestaurantMenu').find({menu_id:{$in:req.body.id}}).toArray((err,result) =>{
+//             if(err) throw err;
+//             res.send(result)
+//         })
+//     }else{
+//         res.send('Inavlid Input')
+//     }
+// })
 
 
-//list of order
-app.get('/orders',(req,res) => {
-    let email = req.query.email
-    let query = {};
-    if(email){
-       // query={email:email}
-        query={email}
-    }
-    db.collection('orders').find(query).toArray((err,result) =>{
-        if(err) throw err;
-        res.send(result)
-    })
-})
+// //list of order
+// app.get('/orders',(req,res) => {
+//     let email = req.query.email
+//     let query = {};
+//     if(email){
+//        // query={email:email}
+//         query={email}
+//     }
+//     db.collection('orders').find(query).toArray((err,result) =>{
+//         if(err) throw err;
+//         res.send(result)
+//     })
+// })
 
-app.put('/updateOrder/:id',(req,res) => {
-    console.log(req.body)
-    console.log(req.params.id)
-    let oid = Number(req.params.id);
-    db.collection('orders').updateOne(
-        {id:oid},
-        {
-            $set:{
-                "status":req.body.status,
-                "bank_name":req.body.bank_name,
-                "date":req.body.date
-            }
-        },(err,result) => {
-            if(err) throw err;
-            res.send('Order Updated')
-        }
-    )
-})
+// app.put('/updateOrder/:id',(req,res) => {
+//     console.log(req.body)
+//     console.log(req.params.id)
+//     let oid = Number(req.params.id);
+//     db.collection('orders').updateOne(
+//         {id:oid},
+//         {
+//             $set:{
+//                 "status":req.body.status,
+//                 "bank_name":req.body.bank_name,
+//                 "date":req.body.date
+//             }
+//         },(err,result) => {
+//             if(err) throw err;
+//             res.send('Order Updated')
+//         }
+//     )
+// })
 
-app.delete('/deleteOrder/:id',(req,res) => {
-    let _id = mongo.ObjectId(req.params.id);
-    db.collection('orders').deleteOne({_id},(err,result) => {
-        if(err) throw err;
-        res.send('Order Deleted')
-    })
-})
+// app.delete('/deleteOrder/:id',(req,res) => {
+//     let _id = mongo.ObjectId(req.params.id);
+//     db.collection('orders').deleteOne({_id},(err,result) => {
+//         if(err) throw err;
+//         res.send('Order Deleted')
+//     })
+// })
 
 
 //connection with mongo
